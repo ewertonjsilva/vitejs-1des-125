332b0baef935f76273c3b1cbd30b057e17022946
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-02-27 22:48:39 -0300

    preparando projeto

---
+++ src/pages/sobre/index.jsx
@@ -0,0 +1,10 @@
+
+export default function Sobre () {
+    return(
+        <>
+            <h1>Sobre</h1>
+            
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore blanditiis rem ullam dolorem iusto, maxime sequi minima, fuga perspiciatis animi alias laudantium tempora in ad minus non repellat est ipsam.</p>
+        </>
+    )
+}
