b6a1dc5cde273f209bb8bc73eb8f48f3b2a3574a
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-13 22:45:55 -0300

    navegacao pronto estilizacao finalizada

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 
 import styles from './index.module.css';
 
@@ -7,6 +8,8 @@ function Home() {
     <div className={styles.container}>
       <div className={styles.containerLista}>
         <h1>Exemplos</h1>
+        <Link to="/exemplo/1">Exemplo 1</Link>
+        <Link to="/exemplo/2">Exemplo 2</Link>
       </div>
       <div className={styles.containerLista}>
         <h1>Atividades</h1>
