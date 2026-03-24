1973271742c8c0a57613b9dfdfceb0b7e3045c38
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-24 20:04:41 -0300

    mockup

---
+++ src/pages/exemplos/ex-13/index.jsx
@@ -0,0 +1,10 @@
+
+export default function Exemplo13 () {
+    return(
+        <>
+            <h1>Exemplo 13</h1>
+            
+            <p>Mokup listar itens</p>
+        </>
+    )
+}
---
+++ src/pages/exemplos/ex-14/index.jsx
@@ -0,0 +1,10 @@
+
+export default function Exemplo14 () {
+    return(
+        <>
+            <h1>Exemplo 1</h1>
+            
+            <p>Mockup manipular itens</p>
+        </>
+    )
+}
