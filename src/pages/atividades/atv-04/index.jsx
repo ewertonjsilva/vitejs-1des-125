a3a733634a8f50070a22b716b7712939e6f88855
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-04-17 14:17:46 -0300

    ajustes exemplo 4 e atividade 5

--- src/pages/atividades/atv-04/index.jsx
+++ src/pages/atividades/atv-04/index.jsx
@@ -1,11 +1,60 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
-export default function Atividade04 () {
-    return(
-        <>
-            <h1 className={styles.titulo}>Atividade 5</h1>
-            
-            <p>Componente mínimo</p>
-        </>
-    )
+function Atividade04() {
+  const [inputValue, setInputValue] = useState({
+    id: '',
+    quantidade: '',
+    produto: ''
+  });
+  const [dadosCadastrados, setDadosCadastrados] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputValue.produto || !inputValue.quantidade) return;
+
+    // Adiciona o novo item e gera um ID único
+    const novoItem = { ...inputValue, id: Date.now() };
+    setDadosCadastrados([...dadosCadastrados, novoItem]);
+
+    // Limpa os campos
+    setInputValue({ id: '', quantidade: '', produto: '' });
+  };
+  
+
+  return (
+    <div className={styles.container}>
+      <h1>Atividade 4 - Lista de compra</h1>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="number"
+          value={inputValue.quantidade}
+          onChange={(e) => setInputValue({ ...inputValue, quantidade: e.target.value })}
+          placeholder="Qtd"
+        />
+        <input
+          type="text"
+          value={inputValue.produto}
+          onChange={(e) => setInputValue({ ...inputValue, produto: e.target.value })}
+          placeholder="Produto..."
+        />
+        <button type="submit">Adicionar</button>
+      </form>
+
+      {dadosCadastrados.length > 0 && <h2>Lista de compras</h2>}
+
+      <ul className={styles.lista}>
+        {dadosCadastrados.map((item) => (
+          <li key={item.id} className={styles.linha}>
+            <span className={styles.conteudo}>
+              {item.quantidade}x {item.produto}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default Atividade04;
